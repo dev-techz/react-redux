c66f0a882d9e5c0b42140227c698106352a62257
Dev Kumar <acc.devkumar@gmail.com>
2023-05-23 17:47:46 +0530

    new setup for redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { useDispatch } from 'react-redux';
 import './App.css';
-import { Counter } from './Components/Count';
+
 
 function App() {
-  const dispach = useDispatch();
   return (
     <div className="App">
-      <button onClick={e=>dispach({type:"increment"})}>Increment</button>
-      <br/>
-      <Counter />
-      <br/>
-      <button onClick={e=>dispach({type:"decrement"})}>Decrement</button>
-    </div>
+      </div>
   );
 }
 
