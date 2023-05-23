7959d50e46a7918e8be37dc33a9ca37aa7bf3c33
Dev Kumar <acc.devkumar@gmail.com>
2023-05-23 18:06:15 +0530

    react toolkit setup on mini add to card  shop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-
+import products from "./products.json";
+import "./App.css";
+import Product from "./Components/Product";
+import Cart from "./Components/Cart";
 
 function App() {
   return (
     <div className="App">
+      <Cart />
+      <div className="products">
+        {products.map((product) => (
+          <Product {...product} />
+        ))}
       </div>
+    </div>
   );
 }
 
