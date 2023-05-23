8661177de3af513baf6095b8a047edfc6eb61d76
Dev Kumar <acc.devkumar@gmail.com>
2023-05-23 17:32:39 +0530

    basic redux setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useDispatch } from 'react-redux';
 import './App.css';
+import { Counter } from './Components/Count';
 
 function App() {
+  const dispach = useDispatch();
   return (
-    <div className="">
-
+    <div className="App">
+      <button onClick={e=>dispach({type:"increment"})}>Increment</button>
+      <br/>
+      <Counter />
+      <br/>
+      <button onClick={e=>dispach({type:"decrement"})}>Decrement</button>
     </div>
   );
 }
